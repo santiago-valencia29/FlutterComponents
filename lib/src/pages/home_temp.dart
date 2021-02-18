236ac71b10c440a0a41979a79dc33feaba45c10e
Santiago Valencia <edy-santiago@outlook.com>
2021-02-17 22:33:51 -0500

    metodo map, listas, icons and on tap

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -8,22 +8,38 @@ class HomePageTemp extends StatelessWidget {
       appBar: AppBar(
         title: Text('Componentes Temp'),
       ),
-      body: ListView(children: _crearItems()),
+      body: ListView(children: _crearItemsCorta()),
     );
   }
 
-  List<Widget> _crearItems() {
-    List<Widget> lista = new List<Widget>();
-    for (String opt in opciones) {
-      final tempWidget = ListTile(
-        title: Text(opt),
-      );
-      lista..add(tempWidget)..add(Divider());
-      //  lista.add(tempWidget);
-      //   lista.add(Divider());
-    }
-    return lista;
-  }
+//Forma tradicional:
+  // List<Widget> _crearItems() {
+  //   List<Widget> lista = new List<Widget>();
+  //   for (String opt in opciones) {
+  //     final tempWidget = ListTile(
+  //       title: Text(opt),
+  //     );
+  //     lista..add(tempWidget)..add(Divider());
+  //     //  lista.add(tempWidget);
+  //     //   lista.add(Divider());
+  //   }
+  //   return lista;
+  // }
 
-  List<Widget> _crearItemsCorta() {}
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item + '       !'),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
+  }
 }
