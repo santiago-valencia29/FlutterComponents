e1423dc8754aec3794efaa6210c7da5a43969f83
Santiago Valencia <edy-santiago@outlook.com>
2021-02-17 21:58:01 -0500

    ListView-show list elements

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Componentes APP',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Componentes'),
-          ),
-          body: HomePageTemp()),
-    );
+        title: 'Componentes APP',
+        debugShowCheckedModeBanner: false,
+        home: HomePageTemp());
   }
 }
